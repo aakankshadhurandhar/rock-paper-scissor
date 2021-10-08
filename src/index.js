482a65ccc0d726550e0022fa342b540567e9c92a
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2021-10-08 09:19:33 +0530

    added play component now images of rock,paper,scissor visible :lipstick:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.scss";
-
+import {BrowserRouter as Router} from "react-router-dom"
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+<Router>
+<App />
+</Router>
+, mountNode);
